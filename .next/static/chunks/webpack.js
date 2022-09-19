b6830d0fc006c7c662002f3d39add786df14eb73
shohan-ch <shohan889@yahoo.com>
2022-09-19 12:57:30 +0600

    type script added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d903a3fefa87e022"; }
+/******/ 		__webpack_require__.h = function() { return "9b5f2ebffce62a1c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
